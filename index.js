4010e7860bcbd87ef02c083d738a409f2f99b033
1san4ik <1san4ik@gmail.com>
2023-07-05 00:02:06 +0300

    small changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-alert('Введіть 3 числа і Ви взнаєте їх середнє арифметичне значення')
+alert('Введіть 3 числа і Ви дізнаєтесь їх середнє арифметичне значення');
 
-let num1 = parseInt(prompt('Введіть перше число'))
-let num2 = parseInt(prompt('Введіть друге число'))
-let num3 = parseInt(prompt('Введіть третє число'))
+let num1 = Number(prompt('Введіть перше число'));
+let num2 = Number(prompt('Введіть друге число'));
+let num3 = Number(prompt('Введіть третє число'));
 
 while (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
-    alert('Одне, або декілька введених значень не Число')
-    num1 = parseInt(prompt('Введіть перше число'))
-    num2 = parseInt(prompt('Введіть друге число'))
-    num3 = parseInt(prompt('Введіть третє число'))
+    alert('Одне або декілька введених значень не є числами');
+    num1 = Number(prompt('Введіть перше число'));
+    num2 = Number(prompt('Введіть друге число'));
+    num3 = Number(prompt('Введіть третє число'));
 }
-let arithmeticMean = (num1 + num2 + num3) / 3
-alert('Середнє арифметичне цих трьох чисел: (' + num1 + ' + ' + num2 + ' + ' + num3 + ') / 3 = ' + arithmeticMean)
+
+let arithmeticMean = (num1 + num2 + num3) / 3;
+alert('Середнє арифметичне цих трьох чисел: (' + num1 + ' + ' + num2 + ' + ' + num3 + ') / 3 = ' + arithmeticMean);
